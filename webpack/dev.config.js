5bdb275a5228a9331a7846357fd4b44ada41570d
MAIALENSARASOLA <maialen.sarasola910@gmail.com>
2026-09-16 13:25:37 +0200

    Fix build script and pin Node 16 for Netlify

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -46,9 +46,10 @@ module.exports = webpackMerge(webpackCommon, {
           {
             loader: 'sass-loader',
             options: {
-              outputStyle: 'expanded',
               sourceMap: true,
-              sourceMapContents: true
+              sassOptions: {
+                outputStyle: 'expanded'
+              }
             }
           }
         ]
